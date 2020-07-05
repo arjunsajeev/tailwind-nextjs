e1f12b9be3bbd2f311f8ec5582e9f8bb690e21df
Arjun Sajeev <arjunsajeev04@gmail.com>
2020-07-05 16:36:46 +0530

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+export default () => (
+  <div className="text-center flex justify-center h-screen items-center flex-col">
+    <h1 className="text-indigo-600 rounded-md shadow font-medium text-3xl leading-relaxed border-indigo-600 border-bottom-2 p-6">
+      Next.js Tailwind starter
+    </h1>
+  </div>
+);
